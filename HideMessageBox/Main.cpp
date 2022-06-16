53cb53d5f3d102dbf682d0d916464e8ebf045386
Serse Dio Re <32899592+SerseDioRe@users.noreply.github.com>
2022-06-16 23:59:47 +0200

    fixed

--- HideMessageBox/Main.cpp
+++ HideMessageBox/Main.cpp
@@ -5,7 +5,24 @@
 
 typedef struct _MSGBOXDATA
 {
-    MSGBOXPARAMSW mbp;          // Size: 0x28 (x86), 0x50 (x64)
+    MSGBOXPARAMSW mbp;          
+    HWND     hwndOwner;         
+#if defined(_WIN32) && (_WIN32_WINNT >= _WIN32_WINNT_WIN7) 
+    DWORD    dwPadding;
+#endif
+    WORD     wLanguageId;
+    INT*     pidButton;            
+    LPCWSTR* ppszButtonText;    
+    DWORD    dwButtons;        
+    UINT     uDefButton;   
+    UINT     uCancelId;       
+#if (_WIN32_WINNT >= _WIN32_WINNT_WINXP) 
+    DWORD    dwTimeout;         
+#endif
+    DWORD    dwReserved0;
+#if (_WIN32_WINNT >= _WIN32_WINNT_WIN7) 
+    DWORD    dwReserved[4];
+#endif
 } MSGBOXDATA, * PMSGBOXDATA, * LPMSGBOXDATA;
 
 typedef __int64(__fastcall* tMessageBoxTimeoutW)(__int64 hWnd, __int64 lpText, __int64 lpCaption, int utype, __int16 a5, int a6);
@@ -68,6 +85,32 @@ uintptr_t SignatureScan(const char* module, const char* pattern)
     return NULL;
 }
 
+void MyMessageBox(LPCWSTR lpszText, LPCWSTR lpszCaption, DWORD dwStyle, LPCWSTR lpszIcon)
+{
+    INT pids[] = { 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13 };
+    LPCWSTR ppText[] = { L"", L"", L"", L"", L"", L"", L"", L"", L"", L"", L"", L"", L"" };
+
+    _MSGBOXDATA msgBoxData;
+    msgBoxData.mbp.cbSize = sizeof(msgBoxData.mbp);
+    msgBoxData.mbp.hwndOwner = 0;
+    msgBoxData.mbp.hInstance = NULL;
+    msgBoxData.mbp.lpszText = lpszText;
+    msgBoxData.mbp.lpszCaption = lpszCaption;
+    msgBoxData.mbp.dwStyle = dwStyle;
+    msgBoxData.mbp.lpszIcon = lpszIcon;
+    msgBoxData.wLanguageId = 0;
+    msgBoxData.pidButton = pids;
+    msgBoxData.ppszButtonText = ppText;
+    msgBoxData.dwButtons = _countof(pids);
+    msgBoxData.uDefButton = 2;
+    msgBoxData.uCancelId = 0;
+#if (_WIN32_WINNT >= _WIN32_WINNT_WINXP) 
+    msgBoxData.dwTimeout = 3 * 1000;
+#endif
+
+    MessageBoxWorker(&msgBoxData);
+}
+
 int main()
 {
     //ShowWindow(GetConsoleWindow(), SW_HIDE);
@@ -77,25 +120,16 @@ int main()
     void* retaddr = nullptr;
     MessageBoxTimeoutW = (tMessageBoxTimeoutW)((uintptr_t)GetProcAddressForCaller(user32HMod, "MessageBoxTimeoutW", retaddr));
 
-    MessageBoxTimeoutW((__int64)0, (__int64)L"MessageBoxTimeoutW", (__int64)L"SUCCESS", MB_ICONINFORMATION, 0, -1);
+    //MessageBoxTimeoutW((__int64)0, (__int64)L"MessageBoxTimeoutW", (__int64)L"SUCCESS", MB_ICONINFORMATION, 0, -1);
 
     MessageBoxWorker = (tMessageBoxWorker)(SignatureScan("user32.dll", "48 89 5C 24 ? 48 89 74 24 ? 48 89 7C 24 ? 55 41 54 41 55 41 56 41 57 48 8D 6C 24 ?"));
-
-    _MSGBOXDATA msgBoxData;
-    msgBoxData.mbp.hwndOwner = 0;
-    msgBoxData.mbp.hInstance = NULL;
-    msgBoxData.mbp.lpszText = L"MessageBoxWorker";
-    msgBoxData.mbp.lpszCaption = L"Success";
-    msgBoxData.mbp.dwStyle = MB_ICONINFORMATION;
-    msgBoxData.mbp.lpszIcon = L"IDI_EXCLAMATION";
-    
-    MessageBoxWorker(&msgBoxData);
+    MyMessageBox(L"MessageBoxWorker", L"Success", 0x00000040L, L"IDI_EXCLAMATION");
 
     std::cout << std::hex << std::uppercase << "GetProcAddressForCaller 0x" << (uintptr_t)GetProcAddressForCaller << '\n';
     std::cout << std::hex << std::uppercase << "MessageBoxTimeoutW 0x" << (uintptr_t)MessageBoxTimeoutW << '\n';
     std::cout << std::hex << std::uppercase << "MessageBoxWorker 0x" << (uintptr_t)MessageBoxWorker << '\n';
 
-    std::cin.get();
+    //std::cin.get();
 
 	return EXIT_SUCCESS;
 }
